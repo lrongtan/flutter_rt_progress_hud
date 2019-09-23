5f3644a65a7915e313c21acb81da66ca26c4fbb4
lrongtan <844928336@qq.com>
2019-09-23 16:58:47 +0800

    dev分支初次上传。这次上传的目的是为了熟练使用git分支。修改了rt_progress_hud.dart文件

--- lib/rt_progress_hud.dart
+++ lib/rt_progress_hud.dart
@@ -1,7 +1,124 @@
 library rt_progress_hud;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
+
+
+
+//菊花半径
+const double _rtDefaultIndicatorRadius = 15.0;
+
+//窗口颜色
+const Color _rtDefaultBgColor = Colors.black12;
+
+//视图颜色
+const Color _rtDefaultContentColor = Colors.black54;
+
+// 视图裁剪半径
+const double _rtDefaultContentRadius = 10.0;
+
+// 视图内边距
+const double _rtDefaultContentPadding = 30;
+
+// label 字体颜色
+const Color _rtDefaultLabelColor = Colors.white;
+
+// label 字体大小
+const double _rtDefaultLabelFontsize = 32.0;
+
+// label 字体行数
+const int _rtDefaultLabelMaxLines = 2;
+
+// detailsLabel 字体颜色
+const Color _rtDefaultDetailsLabelColor = Colors.white;
+
+
+// detailsLabel 字体大小
+const double _rtDefaultDetailLabelFontsize = 30.0;
+
+
+// detailsLabel 字体行数
+const int _rtDefaultDetailLabelMaxLines = 10;
+
+// content最大宽度 这个数据 用在 label 和 detailsLabel上 不包括边距
+const double _rtDefaultContentMaxWidth = 500;
+
+// content最小宽度 这个数据 用在 label 和 detailsLabel上 不包括边距
+const double _rtDefaultContentMinWidth = 200;
+
+//自动pop 的时间
+const int _rtDefaultscheduleDismiss = 1500;
+
+// content内容的内边距
+const double _rtDefaultItemPadding = 20;
+
+// 成功 失败等iconfont 或图片 的宽度
+const double _rtDefaultStateIconWidth = 65;
+
+// ios dialog 的 动画
+final Animatable<double> _dialogScaleTween = Tween<double>(begin: 1.3, end: 1.0)
+    .chain(CurveTween(curve: Curves.linearToEaseOut));
+//const TextStyle _rtDefaultLabelFont = TextStyle(color: Colors.white,fontSize: );
+
+//hud 的风格
+enum HUDStyle {
+  None,// 纯文本
+  Indicator,// 菊花loading
+  Success,//成功
+  Fail,//失败
+  Warn,//警告
+  Custom,//自定义 。。。。未实现
 }
+
+class RTProgressHUD{
+
+  OverlayEntry _overlayEntry;//实现在任意页面都能弹窗的组件
+
+}
+
+
+class RTProgressView extends StatefulWidget {
+
+  HUDStyle mhudStyle;
+
+  String mLabel;
+
+  String mDetailsLabel;
+
+  RTProgressView({this.mhudStyle, this.mLabel, this.mDetailsLabel});
+
+  @override
+  _RTProgressViewState createState() => _RTProgressViewState();
+}
+
+class _RTProgressViewState extends State<RTProgressView> with SingleTickerProviderStateMixin{
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 3000),
+    );
+    super.initState();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
